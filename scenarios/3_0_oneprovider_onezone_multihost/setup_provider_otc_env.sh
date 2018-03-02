f21ad210caadfc82a76f9936a504b489abaa63f8
root <root@machinecd24a24b-db42-4666-a745-0a3f0903b175.novalocal>
2018-03-02 10:44:40 +0000

    Changed otc domains

--- scenarios/3_0_oneprovider_onezone_multihost/setup_provider_otc_env.sh
+++ scenarios/3_0_oneprovider_onezone_multihost/setup_provider_otc_env.sh
@@ -5,90 +5,60 @@ BG=$1
 rhea() {
     echo "export PROVIDER_NAME='RHEA-OTC'"
     echo "export FQDN='68e44e56-machine.vms.nuv.la'"
-    echo "export ZONE_FQDN='onezone.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
 }
 
 cnrs() {
     echo "export PROVIDER_NAME='CNRS-OTC'"
     echo "export FQDN='op-otc-cnrs.rhea-hn.com'"
-    echo "export ZONE_FQDN='onezone-cnrs.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
 }
 
 cern() {
     echo "export PROVIDER_NAME='CERN-OTC'"
     echo "export FQDN='op-otc-cern.rhea-hn.com'"
-    echo "export ZONE_FQDN='onezone-cern.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
 }
 
 desy() {
     echo "export PROVIDER_NAME='DESY-OTC'"
-    echo "export FQDN='op-otc-desy.rhea-hn.com'"
-    echo "export ZONE_FQDN='onezone-desy.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
+    echo "export FQDN='op-otc-desy.hn.nuv.la'"
 }
 
 embl() {
     echo "export PROVIDER_NAME='EMBL-OTC'"
-    echo "export FQDN='op-otc-embl.rhea-hn.com'"
-    echo "export ZONE_FQDN='onezone-embl.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
+    echo "export FQDN='op-otc-embl.hn.nuv.la'"
 }
 
 esrf() {
     echo "export PROVIDER_NAME='ESRF-OTC'"
     echo "export FQDN='op-otc-esrf.rhea-hn.com'"
-    echo "export ZONE_FQDN='onezone-esrf.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
 }
 
 ifae() {
     echo "export PROVIDER_NAME='IFAE-OTC'"
     echo "export FQDN='op-otc-ifae.rhea-hn.com'"
-    echo "export ZONE_FQDN='onezone-ifae.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
 }
 
 infn() {
     echo "export PROVIDER_NAME='INFN-OTC'"
-    echo "export FQDN='op-otc-infn.rhea-hn.com'"
-    echo "export ZONE_FQDN='onezone-infn.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
+    echo "export FQDN='op-otc-infn.hn.nuv.la'"
 }
 
 kit() {
     echo "export PROVIDER_NAME='KIT-OTC'"
     echo "export FQDN='op-otc-kit.rhea-hn.com'"
-    echo "export ZONE_FQDN='onezone-kit.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
 }
 
 stfc() {
     echo "export PROVIDER_NAME='STFC-OTC'"
     echo "export FQDN='op-otc-stfc.stfc-hn.com'"
-    echo "export ZONE_FQDN='onezone-stfc.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
 }
 
 surfsara() {
     echo "export PROVIDER_NAME='SURFSara-OTC'"
-    echo "export FQDN='op-otc-surfsara.rhea-hn.com'"
-    echo "export ZONE_FQDN='onezone-surfsara.rhea-hn.com'"
-    mkdir -p /opt/oneprovider/persistence
-    mkdir -p /opt/oneprovider/storage
+    echo "export FQDN='op-otc-surfsara.hn.nuv.la'"
 }
+
+mkdir -p /opt/oneprovider/persistence
+mkdir -p /opt/oneprovider/storage
 
 IP=$(curl -sS http://ipinfo.io/ip)
 GEO_LATITUDE=$(curl -s --get http://freegeoip.net/json/$IP | grep -Po '(?<="latitude":)[^,}]*')
@@ -100,5 +70,7 @@ echo "export ADMIN_PASSWORD=$ADMIN_PASSWORD"
 
 echo "export GEO_LATITUDE=$GEO_LATITUDE"
 echo "export GEO_LONGITUDE=$GEO_LONGITUDE"
+
+echo "export ZONE_FQDN='onezone.rhea-hn.com'"
 
 $BG
